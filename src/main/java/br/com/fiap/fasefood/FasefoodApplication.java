10a7334d38cdc84fb7198731138bf822d6b32927
Marcos Felisberto <marcossfelisberto@gmail.com>
2025-04-11 08:01:48 -0300

    Criando Repositorio

---
+++ src/main/java/br/com/fiap/fasefood/FasefoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fasefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasefoodApplication.class, args);
+	}
+
+}
