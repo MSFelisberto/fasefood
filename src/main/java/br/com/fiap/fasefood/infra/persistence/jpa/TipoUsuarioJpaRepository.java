b4511722e2197760d30364513fc1d299e44be084
Marcos Felisberto <marcossfelisberto@gmail.com>
2025-07-27 13:39:49 -0300

    Ajuste na criacao de usuario e do tipo usuario para o consumidor mandar o nome do tipo usuario através da criação de um novo usuario.

--- src/main/java/br/com/fiap/fasefood/infra/persistence/jpa/TipoUsuarioJpaRepository.java
+++ src/main/java/br/com/fiap/fasefood/infra/persistence/jpa/TipoUsuarioJpaRepository.java
@@ -3,5 +3,8 @@ package br.com.fiap.fasefood.infra.persistence.jpa;
 import br.com.fiap.fasefood.infra.persistence.entities.TipoUsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TipoUsuarioJpaRepository extends JpaRepository<TipoUsuarioEntity, Long> {
+    Optional<TipoUsuarioEntity> findByNome(String nome);
 }
