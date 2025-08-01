184057b38f35745a4ad7227c48e1a6f73775fafd
Heider Soares <heiderbezerra18@gmail.com>
2025-08-01 01:25:31 -0300

    Finalizando o dia 01/08

---
+++ src/test/java/br/com/fiap/fasefood/application/usecace/restaurante/ListarRestaurantesUseCaseImplTest.java
@@ -0,0 +1,4 @@
+package br.com.fiap.fasefood.application.usecace.restaurante;
+
+public class ListarRestaurantesUseCaseImplTest {
+}
---
+++ src/test/java/br/com/fiap/fasefood/application/usecace/usuario/AtualizarUsuarioUseCaseImplTest.java
@@ -0,0 +1,4 @@
+package br.com.fiap.fasefood.application.usecace.usuario;
+
+public class AtualizarUsuarioUseCaseImplTest {
+}
